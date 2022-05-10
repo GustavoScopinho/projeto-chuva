1136bfbcd49647e94e589ce06562f2ec41b88330
GustavoScopinho <guh.fs@hotmail.com>
2022-05-10 15:36:01 -0300

    Componente de header do conteudo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import GlobalStyles from "./styles/globalStyles";
 import { SideBar } from "./components/SideBar";
 import { Header } from "./components/Header";
+import { ContentHeader } from "./components/ContentHeader";
 
 function App() {
   return (
     <>
       <div style={{ display: "flex", flexDirection: "row" }}>
         <SideBar />
-        <Header />
+        <div
+          style={{ display: "flex", flexDirection: "column", width: "100%" }}
+        >
+          <Header />
+          <ContentHeader />
+        </div>
       </div>
       <GlobalStyles />
     </>
